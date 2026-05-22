241dcdcbb081a070794952e646c8d1407d5b9e11
Rashmin <rashminxperience@gmail.com>
2026-05-22 21:23:44 +0545

    test

--- lib/auth.ts
+++
@@ -1,7 +0,0 @@
-import { signIn, signOut } from "@/auth";
-export const login = async () => {
-  await signIn("github", { redirectTo: "/" });
-};
-export const logout = async () => {
-  await signOut({ redirectTo: "/auth/signin" });
-};
